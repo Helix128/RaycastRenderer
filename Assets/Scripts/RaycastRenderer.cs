d88685281991b9493822a32d0fb43175e8488597
Helix <diegoatari8@mail.com>
2020-05-17 19:43:18 -0400

    Added texture support and custom resolution major fixes.

--- Assets/Scripts/RaycastRenderer.cs
+++ Assets/Scripts/RaycastRenderer.cs
@@ -7,17 +7,20 @@ public class RaycastRenderer : MonoBehaviour
     public RawImage Target;
     public int width;
     public int height;
+    public bool AutoStepSize;
+    public float stepSizeX = 1;
+    public float stepSizeY = 1;
     public Light Sun;
-
+    public Color Background;
     public bool ShadeBounce;
     public bool ReflectionBounce;
- 
     public float ReflectionRayLength;
     Vector3 SunDir;
-    int pixelCount;
-    public bool Realtime;
+
+    public bool Render;
     public int i;
     public int h;
+    int tracedPixels;
     public Vector3 TracingOffset;
     float Timer;
     Texture2D Final;
@@ -27,39 +30,52 @@ public class RaycastRenderer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        RaycastRender();
-
+        tracedPixels = 0;
+        //Setup the target texture
         Final = new Texture2D(width, height);
         Final.filterMode = FilterMode.Point;
+
+        //Init rendering
+        RaycastRender();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Target.GetComponent<AspectRatioFitter>().aspectRatio = width / height;
+        if (AutoStepSize)
+        {
+            stepSizeX = width / 64;
+            stepSizeY = height / 64;
+
+        }
         SunDir = Sun.transform.forward;
         if (Mathf.Abs(pos.magnitude - transform.position.magnitude)>0.0001f||Mathf.Abs(euler.magnitude-transform.eulerAngles.magnitude)>0.0001f)
-        {
+        { 
             i = 0;
             h = 0;
             Final = new Texture2D(width, height);
             Final.filterMode = FilterMode.Point;
-            Final.Apply();  
+            Final.Apply();
+            tracedPixels = 0;
+            Render = true;
         }
-        if (Realtime)
-        {
-            RaycastRender();
-        }   
+      
     }
     private void FixedUpdate()
     {
         Timer -= Time.fixedDeltaTime;
         if (Timer < 0)
         {
-            Timer = 0.5f;
+            Timer = 1f;
             pos = transform.position;
             euler = transform.eulerAngles;
+         
         }
-     
+        if (Render)
+        {
+            RaycastRender();
+        }
     }
     void RaycastRender()
     {
@@ -69,19 +85,35 @@ public class RaycastRenderer : MonoBehaviour
         RaycastHit shit;
         RaycastHit ghit;
         RaycastHit rhit;
-        Debug.DrawRay(TracingOffset+CamRay.origin+transform.up*(h-width/2)/ Camera.main.fieldOfView, CamRay.direction + transform.right * i * Camera.main.fieldOfView / 2000 + transform.up * .01f + transform.up * (h - height / 4)/height, Color.red,8f);
-
-        if (Physics.Raycast(TracingOffset + CamRay.origin + transform.up * (h - width / 2) / Camera.main.fieldOfView, CamRay.direction + transform.right * i * Camera.main.fieldOfView / 2000 + transform.up * .01f + transform.up * (h - height / 4) / height, out hit))
+        Debug.DrawRay(TracingOffset+CamRay.origin+(transform.up*(h-width/2)/ Camera.main.fieldOfView)/stepSizeY, CamRay.direction + (transform.right * i)/stepSizeX * Camera.main.fieldOfView / 2000 + ( transform.up * .01f)/stepSizeY + (transform.up * (h - height / 4)/height)/stepSizeY, Color.red,8f);
+        tracedPixels++;
+        if (Physics.Raycast(TracingOffset + CamRay.origin + (transform.up * (h - width / 2) / Camera.main.fieldOfView) / stepSizeY, CamRay.direction + (transform.right * i) / stepSizeX * Camera.main.fieldOfView / 2000 + (transform.up *stepSizeX/5) / stepSizeY + (transform.up * (h - height / 4) / height) / stepSizeY, out hit))
         {
+            MeshRenderer rend = hit.collider.GetComponent<MeshRenderer>();
+            Texture2D texture2D = rend.sharedMaterial.mainTexture as Texture2D;
+            Vector2 pixelUV = hit.textureCoord;
+            pixelUV.x *= texture2D.width;
+            pixelUV.y *= texture2D.height;
+            Vector2 tiling = rend.sharedMaterial.mainTextureScale;
+            if (texture2D != null && texture2D.isReadable)
+            {
+                Final.SetPixel(i, h, texture2D.GetPixel((int)pixelUV.x*(int)tiling.x,(int)pixelUV.y*(int)tiling.y) * rend.sharedMaterial.color);
+                Final.Apply();
 
-            Final.SetPixel(i, h, hit.collider.GetComponent<MeshRenderer>().sharedMaterial.color);
+            }
+            else
+            {
+                Final.SetPixel(i, h, rend.sharedMaterial.color);
+                Final.Apply();
+
+            }
 
 
-            Final.Apply();
-       
+
+
             if (ShadeBounce)
             {
-                Debug.DrawRay(hit.point + hit.normal * 0.02f, -SunDir, Color.yellow);
+                Debug.DrawRay(hit.point + hit.normal * 0.02f, -SunDir*100, Color.yellow);
                 if (Physics.Raycast(hit.point + hit.normal * 0.02f, -SunDir, out shit,Mathf.Infinity))
                 {
                     Final.SetPixel(i, h,Final.GetPixel(i,h)*RenderSettings.ambientLight);
@@ -91,7 +123,7 @@ public class RaycastRenderer : MonoBehaviour
                 {
                     if (ReflectionBounce)
                     {
-                        Debug.DrawRay(hit.point + hit.normal * 0.02f, hit.normal, Color.yellow);
+                        Debug.DrawRay(hit.point + hit.normal * 0.02f, hit.normal*100, Color.yellow);
                         if (Physics.Raycast(hit.point + hit.normal * 0.02f, hit.normal, out ghit,ReflectionRayLength))
                         {
                             Final.SetPixel(i, h,Final.GetPixel(i, h)*(ghit.collider.GetComponent<MeshRenderer>().sharedMaterial.color));
@@ -104,7 +136,7 @@ public class RaycastRenderer : MonoBehaviour
             {
                 if (ReflectionBounce)
                 {
-                    Debug.DrawRay(hit.point + hit.normal * 0.02f, hit.normal, Color.yellow);
+                    Debug.DrawRay(hit.point + hit.normal * 0.02f, hit.normal*100, Color.yellow);
                     if (Physics.Raycast(hit.point + hit.normal * 0.02f, hit.normal, out rhit, ReflectionRayLength))
                     {
                         Final.SetPixel(i, h, Final.GetPixel(i, h) * (rhit.collider.GetComponent<MeshRenderer>().sharedMaterial.color));
@@ -113,6 +145,11 @@ public class RaycastRenderer : MonoBehaviour
                 }
             }
           
+        }
+        else
+        {
+            Final.SetPixel(i, h,Background);
+            Final.Apply();
         }
   
         
@@ -126,7 +163,12 @@ public class RaycastRenderer : MonoBehaviour
         {
             h = 0;
         }
+        if (tracedPixels >= width * height)
+        {
 
+            Render = false;
+
+        }
         Target.texture = Final;
     }
      
--- README.md
+++ README.md
@@ -1,19 +1,15 @@
 # CPURaycasterUnity
- Raycasting Render experiment in Unity
+ Raycast Rendering experiment in Unity
  Done in Unity 2019.3.9f1.
 
 This was done just as an experiment,i know there are lots of more efficient ways of doing this (Like Compute shaders with actual Ray Tracing),but i made this as my first time making a custom render method like this.
 I hope you can find this useful,and if you find any way to enhance or optimize this,please let me know. Thanks!
 
 # Functionality
--Main Render Ray (Color only)
+-Main Render Ray (Color & Texture)
 
 -Shadow Ray
 
 -Reflection Ray
 
-# Planned additions
 
--Texture support for render ray
-
--Higher resolution support (as of now it only works in 64x64,if set to higher or lower the result is distorted)
